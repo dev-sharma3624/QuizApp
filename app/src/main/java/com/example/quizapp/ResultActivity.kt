159c7fecc04fa9f717b4164246439b03c59f7e0e
dev-sharma3624 <devsharma.3624@gmail.com>
2024-08-29 10:45:07 +0530

    Uploading source code to remote repo

---
+++ app/src/main/java/com/example/quizapp/ResultActivity.kt
@@ -0,0 +1,17 @@
+package com.example.quizapp
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        val tvName: TextView = findViewById(R.id.tv_name)
+    }
+}
